fc6668f6e4405fc595493a327acfaeb6c1f86e69
tanonkim <robbins4bos@gmail.com>
2025-08-08 21:09:06 +0900

    feat : modal with intercept-route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
     children,
+    modal,
 }: Readonly<{
     children: React.ReactNode;
+    modal: React.ReactNode;
 }>) {
     return (
         <html className={`h-screen min-h-screen leading-none ${chaney.variable}`} lang="ko">
-            <body className=" h-full w-full font-chaney">{children}</body>
+            <body className="h-full w-full font-chaney">
+                {children}
+                {modal}
+                <div id="modal-root" />
+            </body>
         </html>
     );
 }
